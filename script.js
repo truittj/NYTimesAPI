76bee354fb9138ea30cf8bbf7f05b0b6d2ea3e50
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-15 14:15:10 -0500

    got the nyc times API to work. Still need variables

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 //$("button").on("click", function () {
 //var ### = #######;
-var queryURL =
-  "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=75Mjorzdv1647cioSWc0tteOxTcAkjbp" +
-  //   person +
-  //   "&api-key=75Mjorzdv1647cioSWc0tteOxTcAkjbp";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-  });
+var APIkey = "75Mjorzdv1647cioSWc0tteOxTcAkjbp";
+var userIput = ###
+var numberOfRecords = ###
+var startDate = ###
+var finishDate = ###
+var queryURL =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+  userIput +
+  "&api-key=" +
+  APIkey;
+
+//   person +
+//   "&api-key=75Mjorzdv1647cioSWc0tteOxTcAkjbp";
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
